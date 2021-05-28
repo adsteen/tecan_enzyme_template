273c8ccfd9b76978cc6b9ce1ce7186b348792a89
Andrew Steen <andrew.decker.steen@gmail.com>
2021-05-28 11:30:42 -0400

    initial commit

---
+++ R/example_tecan_analysis.R
@@ -0,0 +1,16 @@
+##########
+# Example script to analyze enzyme data from Tecan reader
+# We're going to analyze some example data from Brianna
+# This is plate 1:
+# Core ______, depth 0_75_bottom
+##########
+
+# This would be a good place to explain what the depth naming scheme means
+
+#####
+# Load packages
+####
+library(tidyverse)
+
+# 
+
